75924084a72d871096bb900e211e8d47562d75c5
Akihisa Yagi <capra314cabra@gmail.com>
2021-07-02 12:04:41 +0900

    Extract OrderItem from OrderList

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -1,61 +1,85 @@
 import React from "react";
-import { H1, Card, CardItem, Body, Text } from "native-base";
+import { H1, Text, Button } from "native-base";
 import { StyleSheet, View } from "react-native";
 
-import { IOrderInfo, OrderStatus } from "../models/OrderInfo";
+import { OrderItem } from "./OrderItem";
+import { IOrderInfo, ordersDatabase, OrderStatus } from "../models/OrdersDB";
 
 /**
  * Styles for `OrderList`.
  */
-const orderListStyles = StyleSheet.create({
+ const orderListStyles = StyleSheet.create({
     container: {
         alignItems: "center"
     },
     title: {
         margin: 30
     },
-    card: {
-        width: "90%"
+    button: {
+        marginTop: 30,
+        marginLeft: "3%",
+        marginRight: "3%",
+        width: "44%"
     }
 });
 
 /**
- * Properties of `OrderItem`.
+ * Properties of `OrderList`.
  */
-interface IOrderItemProps {
-    order: IOrderInfo;
-}
+interface IOrderListProps { }
 
 /**
- * This is an UI which displays information of an order.
+ * State of `OrderList`.
  */
- export class OrderItem extends React.Component<IOrderItemProps> {
-    render() {
-        return (
-            <Card style={orderListStyles.card}>
-                <CardItem>
-                    <Body>
-                        <Text>
-                            {this.props.order.tableID}番テーブルのオーダー
-                        </Text>
-                    </Body>
-                </CardItem>
-            </Card>
-        );
-    }
+interface IOrderListState {
+    orders: IOrderInfo[];
 }
 
 /**
  * This is an UI which displays items ordered.
  */
-export class OrderList extends React.Component {
+export class OrderList extends React.Component<IOrderListProps, IOrderListState> {
+    constructor(props: IOrderListProps) {
+        super(props);
+
+        this.state = {
+            orders: []
+        };
+
+        this.loadOrders();
+    }
+
+    async loadOrders() {
+        let orders = await ordersDatabase.get(OrderStatus.POSTED)
+
+        this.setState({
+            orders: orders
+        });
+    }
+
     render() {
         return (
             <View style={orderListStyles.container}>
                 <H1 style={orderListStyles.title}>
                     現在の注文
                 </H1>
-                <OrderItem order={{ id: 0, tableID: 0, date: new Date(), orders: [], status: OrderStatus.READY }} />
+                {
+                    this.state.orders
+                        .map((val) => {
+                            return (
+                                <OrderItem order={val}>
+                                    <View style={{ width: "100%", flexDirection: "row" }}>
+                                        <Button success block style={orderListStyles.button}>
+                                            <Text>準備完了</Text>
+                                        </Button>
+                                        <Button danger block style={orderListStyles.button}>
+                                            <Text>取り消し</Text>
+                                        </Button>
+                                    </View>
+                                </OrderItem>
+                            );
+                        })
+                }
             </View>
         );
     }
