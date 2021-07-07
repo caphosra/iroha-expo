615b110e28c59903d9e37b3bd1881c8a0f21e871
Akihisa Yagi <capra314cabra@gmail.com>
2021-07-07 12:15:04 +0900

    Move Font.loadAsync from MainContent to App

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,9 +1,6 @@
-import * as Font from "expo-font";
-import { preventAutoHideAsync, hideAsync } from "expo-splash-screen";
 import { Container, Content } from "native-base";
 import React from "react";
 import { Route, MemoryRouter, Switch } from "react-router-native";
-
 import { IrohaFooterWithRouter } from "./components/Footer";
 import { IrohaHeader } from "./components/Header";
 import { HomeContent } from "./components/HomeContent";
@@ -11,41 +8,11 @@ import { NoticeList } from "./components/NoticeList";
 import { OrderList } from "./components/OrderList";
 import { TakeOrderWithRouter } from "./components/TakeOrder";
 
-interface IMainContentProps { }
-
-interface IMainContentState {
-    isReady: boolean;
-}
-
 /**
  * A component which works as the root and has all of the components.
  */
-export class MainContent extends React.Component<IMainContentProps, IMainContentState> {
-    constructor(props: IMainContentProps) {
-        super(props);
-        this.state = {
-            isReady: false,
-        };
-    }
-
-    async componentDidMount() {
-        await preventAutoHideAsync();
-        await Font.loadAsync({
-            Roboto: require("native-base/Fonts/Roboto.ttf"),
-            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-            Ionicons: require("native-base/Fonts/Ionicons.ttf")
-        });
-        this.setState({
-            isReady: true
-        });
-        await hideAsync();
-    }
-
+export class MainContent extends React.Component {
     render() {
-        if (!this.state.isReady) {
-            return null;
-        }
-
         return (
             <Container>
                 <MemoryRouter>
